82624013e18b206a8c9d33db4c0ee7521e869888
Thinh Nguyen Xuan <toi.la.dan.xu.nghe@gmail.com>
2020-10-31 08:45:58 +0700

    update SignIn

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Breadcrumb, Form, Input, Button, Checkbox } from 'antd';
 import { Helmet } from 'react-helmet';
 
+const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+  const tailLayout = {
+    wrapperCol: {
+      offset: 8,
+      span: 16,
+    },
+  };
+
 const SignIn = () => {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+      };
+    
+      const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+      };
+
     return (
         <>
             <Helmet>
@@ -13,8 +36,54 @@ const SignIn = () => {
                 <Breadcrumb.Item>SignIn</Breadcrumb.Item>
             </Breadcrumb>
             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                Bill is a cat.
+            <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
               </div>
+              
+              
         </>
     );
 };
